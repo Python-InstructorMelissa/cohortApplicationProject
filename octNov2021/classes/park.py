d5eefc49d1aedf91b39e133df0741e8acdaad8f7
melissa-24 <melissalongenberger24@gmail.com>
2021-10-15 20:33:25 -0400

    started project with everything learned through week 3

--- octNov2021/classes/park.py
+++ octNov2021/classes/park.py
@@ -11,3 +11,23 @@ class Park:
         self.animals = []
         self.plant = []
         self.employees = []
+
+    def returnData(self):
+        parks = self
+        return parks
+
+park = []
+
+
+dojo=Park("Dojo Park", "Virtual", "Educational", 49.95, "24/7", "Not Required")
+dojoPark = Park.returnData(dojo)
+
+coding=Park("Dojo Zoo", "Jacksonville, FL", "Zoo", 30.50, "M-F 4pm-8pm, Saturdays 10am-8pm, Closed Sundays", "Onsite parking $5 per car")
+codingPark = Park.returnData(coding)
+
+
+park.append(dojoPark)
+park.append(codingPark)
+
+
+
--- octNov2021/server.py
+++ octNov2021/server.py
@@ -1,17 +1,24 @@
-from flask import Flask, render_template
-from classes.park import Park
+from flask import Flask, render_template, session, redirect, request
+from classes.park import *
+from env import KEY
 
 parkApp = Flask(__name__)
 
-
+parkApp.secret_key = KEY
 
 @parkApp.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', park=park)
 
 @parkApp.route('/about')
 def about():
     return render_template('about.html')
+
+@parkApp.route('/addAbout', methods=['POST'])
+def createAbout():
+    session['name'] = request.form['name']
+    session['description'] = request.form['description']
+    return redirect('/about')
 
 
 
